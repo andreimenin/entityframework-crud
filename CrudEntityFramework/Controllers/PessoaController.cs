cabfadafc281eb306ef53bdbb3fa63aa30230fd9
Andrei Menin <andreimenin@gmail.com>
2022-01-05 21:33:29 -0300

    correções no css e tratativa de dados

---
+++ CrudEntityFramework/Controllers/PessoaController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TesteCrud.Models;
+namespace TesteCrud.Controllers
+{
+    public class PessoaController : Controller
+    {
+        // GET: Pessoa  
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Fetch Data From Database to show in Datatable  
+        public ActionResult GetData()
+        {
+            using (DBModel db = new DBModel())
+
+            {
+                //filtrando pessoas ativas no sistema
+                var pessoasAtivas = db.Pessoas.Where(a => a.Ativo == true);
+                List<Pessoa> pessoaList = pessoasAtivas.ToList<Pessoa>();
+
+                return Json(new { data = pessoaList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        //Create Method for Insert and Update  
+
+        [HttpGet]
+        public ActionResult StoreOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new Pessoa());
+            else
+            {
+                using (DBModel db = new DBModel())
+                {
+
+
+                    return View(db.Pessoas.Where(x => x.Codigo == id).FirstOrDefault<Pessoa>());
+
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult StoreOrEdit(Pessoa pessoaObj)
+        {
+            using (DBModel db = new DBModel())
+            {
+                if (pessoaObj.Codigo == 0)
+                {
+                    pessoaObj.Ativo = true;
+                    db.Pessoas.Add(pessoaObj);                    
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Salvo com sucesso!", JsonRequestBehavior.AllowGet });
+                }
+                else
+                {
+                    pessoaObj.Ativo = true;
+                    db.Entry(pessoaObj).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Json(new { success = true, message = pessoaObj.Codigo + " atualizado com sucesso!", JsonRequestBehavior.AllowGet });
+                }
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)        
+        {
+            using (DBModel db = new DBModel())
+            {
+                Pessoa pessoaObj = db.Pessoas.Where(x => x.Codigo == id).FirstOrDefault<Pessoa>();
+                //db.Pessoas.Remove(emp);
+                pessoaObj.Ativo = false;
+                db.Entry(pessoaObj).State = EntityState.Modified;
+                db.SaveChanges();                
+                return Json(new { success = true, message = pessoaObj.Codigo+" excluído com sucesso", JsonRequestBehavior.AllowGet });
+            }
+        }
+
+    }
+}
---
+++ CrudEntityFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As Informações Gerais sobre um assembly são controladas por meio do 
+// conjunto de atributos a seguir. Altere esses valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("TesteCrud")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TesteCrud")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Configurar o ComVisible como false torna os tipos desse assembly invisíveis 
+// para componentes COM.  Se for necessário acessar um tipo nesse assembly a partir do 
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// A GUID a seguir será referente à ID do typelib se este projeto for exposto ao COM
+[assembly: Guid("8ca85884-f525-4249-aea9-0fad4aba0a19")]
+
+// As informações de versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou utilizar como padrão os Números de Revisão e da Versão 
+// usando o '*' como mostrado abaixo:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
